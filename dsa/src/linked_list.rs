5589a84e2a063f04b545b043cb9ed5875da39190
mickaelyoshua <mickaelyoshua@gmail.com>
2026-06-15 15:10:26 -0300

    implement core linked list operations
    - fix generic type definitions for link and node
    - add push_front, pop_front, push_back and pop_back
    - derive default trait for linkedlist

--- dsa/src/linked_list.rs
+++ dsa/src/linked_list.rs
@@ -1,16 +1,61 @@
-type Link = Option<Box<Node<T>>>
+pub type Link<T> = Option<Box<Node<T>>>;
 
-struct Node<T> {
-    val: T,
-    next: Link,
+pub struct Node<T> {
+    pub val: T,
+    pub next: Link<T>,
+}
+impl<T> Node<T> {
+    fn new(val: T) -> Self {
+        Self { val, next: None }
+    }
 }
 
+#[derive(Default)]
 pub struct LinkedList<T> {
-    pub head: Link,
+    pub head: Link<T>,
 }
 
-impl LinkedList<T> {
-    pub fn new(val: T) -> Self {
-        uninplemented!()
+impl<T> LinkedList<T> {
+    pub fn push_front(&mut self, val: T) {
+        let old_head = self.head.take();
+        let new_node = Box::new(Node {
+            val,
+            next: old_head,
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let old_head = self.head.take();
+        match old_head {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.val)
+            }
+        }
+    }
+
+    pub fn push_back(&mut self, val: T) {
+        let mut current_link = &mut self.head;
+        while let Some(node) = current_link {
+            current_link = &mut node.next;
+        }
+
+        let new_node = Box::new(Node::new(val));
+        *current_link = Some(new_node);
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.head.as_ref()?;
+
+        let mut current_link = &mut self.head;
+        while current_link.as_ref().unwrap().next.as_ref().is_some() {
+            current_link = &mut current_link.as_mut().unwrap().next;
+        }
+
+        let node = current_link.take().unwrap();
+        Some(node.val)
     }
 }
