a63e869f62c6b2084ad702f60a70aad1ffe75d1e
mickaelyoshua <mickaelyoshua@gmail.com>
2026-06-15 16:46:39 -0300

    add tests and iterative drop to linkedlist
    - implement custom Drop trait to prevent stack overflow
    - add proptest dependency for property-based testing
    - add tests for push/pop operations and drop behavior

--- dsa/src/linked_list.rs
+++ dsa/src/linked_list.rs
@@ -1,5 +1,6 @@
 pub type Link<T> = Option<Box<Node<T>>>;
 
+#[derive(Debug)]
 pub struct Node<T> {
     pub val: T,
     pub next: Link<T>,
@@ -10,12 +11,16 @@ impl<T> Node<T> {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct LinkedList<T> {
     pub head: Link<T>,
 }
 
 impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
     pub fn push_front(&mut self, val: T) {
         let old_head = self.head.take();
         let new_node = Box::new(Node {
@@ -57,5 +62,97 @@ impl<T> LinkedList<T> {
 
         let node = current_link.take().unwrap();
         Some(node.val)
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut current_link = self.head.take();
+
+        while let Some(mut node) = current_link {
+            current_link = node.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_empty() {
+        let list: LinkedList<i32> = LinkedList::new();
+        assert!(list.head.is_none());
+    }
+
+    #[test]
+    fn pop_empty_return_none() {
+        let mut list: LinkedList<i32> = LinkedList::new();
+
+        assert_eq!(list.pop_back(), None);
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn push_and_pop_front() {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        list.push_front(2);
+
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+    }
+
+    #[test]
+    fn push_and_pop_back() {
+        let mut list = LinkedList::new();
+
+        list.push_back(1);
+        list.push_back(2);
+
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.pop_back(), Some(1));
+    }
+
+    #[test]
+    fn drop_without_stack_overflow() {
+        let mut list = LinkedList::new();
+
+        for i in 0..100_000 {
+            list.push_front(i);
+        }
+    }
+
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn prop_push_pop_front(items in prop::collection::vec(any::<i32>(), 0..100)) {
+            let mut list = LinkedList::new();
+
+            for &item in items.iter() {
+                list.push_front(item);
+            }
+            for &item in items.iter().rev() {
+                assert_eq!(list.pop_front(), Some(item));
+            }
+
+            assert_eq!(list.pop_front(), None);
+        }
+
+        #[test]
+        fn prop_push_pop_back(items in prop::collection::vec(any::<i32>(), 0..100)) {
+            let mut list = LinkedList::new();
+
+            for &item in items.iter() {
+                list.push_back(item);
+            }
+            for &item in items.iter().rev() {
+                assert_eq!(list.pop_back(), Some(item));
+            }
+
+            assert_eq!(list.pop_back(), None);
+
+        }
     }
 }
