efde9660f32d031744872cbbd41197c44fe6ce58
mickaelyoshua <mickaelyoshua@gmail.com>
2026-06-17 12:41:01 -0300

    implement is unique using hashmap and hashset
    - adds O(N) solution using HashMap and Entry API
    - adds O(N) solution using HashSet
    - adds base case and happy path tests

--- ch_01_arrays_and_strings/1_1_is_unique/src/lib.rs
+++ ch_01_arrays_and_strings/1_1_is_unique/src/lib.rs
@@ -1,1 +1,76 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+// Since it is mandatory to go through the entire string to check if it has unique characters, the
+// BCR (Best Conceivable Runtime) is O(N)
+use std::collections::{HashMap, HashSet, hash_map::Entry};
+
+pub fn is_all_unique_hash_map(s: &str) -> bool {
+    if s.is_empty() {
+        return true;
+    }
+
+    let mut map = HashMap::new();
+    // O(N)
+    for c in s.chars() {
+        // entry for in place manipulation
+        if let Entry::Vacant(e) = map.entry(c) {
+            e.insert(1);
+        } else {
+            return false;
+        }
+    }
+
+    true
+}
+
+// HashSet stores only keys without values.
+// Based on sets. A collections of unique values.
+pub fn is_all_unique_hash_set(s: &str) -> bool {
+    if s.is_empty() {
+        return true;
+    }
+
+    let mut set = HashSet::new();
+    // O(N)
+    for c in s.chars() {
+        if !set.insert(c) {
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn is_all_unique_no_ds(s: &str) -> bool {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn run_tests_for(f: fn(&str) -> bool) {
+        // Happy path
+        assert!(!f("foo"), "Falhou para 'foo'");
+        assert!(f("bar"), "Falhou para 'bar'");
+
+        // Base cases
+        assert!(f(""), "Falhou para string vazia");
+        assert!(f("a"), "Falhou para caractere único");
+    }
+
+    #[test]
+    fn test_hash_map() {
+        run_tests_for(is_all_unique_hash_map);
+    }
+
+    #[test]
+    fn test_hash_set() {
+        run_tests_for(is_all_unique_hash_set);
+    }
+
+    #[test]
+    fn test_no_ds() {
+        run_tests_for(is_all_unique_no_ds);
+    }
+}
