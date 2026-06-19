3fde8b0fab11a2e591d581b70c6a00a3d478d7f0
mickaelyoshua <mickaelyoshua@gmail.com>
2026-06-19 08:35:45 -0300

    init 1.3 urlify base project

---
+++ ch_01_arrays_and_strings/1_3_urlify/src/lib.rs
@@ -0,0 +1,23 @@
+// Write a method to replace all spaces in a string with '%20'. You may assume that the string
+// has sufficient space at the end to hold the additional characters, and that you are given the "true"
+// length of the string. (Note: If implementing in Java, please use a character array so that you can
+// perform this operation in place.)
+//
+// EXAMPLE
+// Input:  "Mr John Smith    ", 13
+// Output: "Mr%20John%20Smith"
+
+pub fn urlify(s: &mut [char], true_length: usize) {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore = "a ser implementado para teste"]
+    fn test_urlify() {
+        // Seu teste aqui
+    }
+}
